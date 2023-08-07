d287b7276fd4ef70126c4fc18936852ff4138bb3
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-08-07 12:18:26 +0530

    ~.~

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-export default function page() {
-  return <div>page</div>;
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+      <Navbar />
+      </main>
+      <Footer />
+    </div>
+  );
 }
