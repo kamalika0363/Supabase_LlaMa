4821eedc9b74bd56ec3a67ca35ea45c5b1b8edb6
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-08-12 13:54:19 +0530

    Frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import LinkBox from "@/components/LinkBox";
+import SummaryBox from "@/components/SummaryBox";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
       <Navbar />
+      <div className="flex flex-row p-6 gap-8">
+      <LinkBox />
+      <SummaryBox /> 
+      </div> 
       </main>
       <Footer />
     </div>
---
+++ components/LinkBox.tsx
@@ -0,0 +1,43 @@
+"use client"
+import React, { useState } from 'react';
+import Icon from '../public/Icon.svg';
+
+const LinkBox: React.FC = () => {
+  const [links, setLinks] = useState<string[]>([]);
+
+  const handleAddLink = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const input = event.currentTarget.elements.namedItem(
+      'link-input'
+    ) as HTMLInputElement;
+    setLinks((prevLinks) => [...prevLinks, input.value]);
+    input.value = '';
+  };
+
+  return (
+    <div className="bg-gradient-to-r from-[#161617] to-[#212020] p-6 rounded-xl flex flex-col item-center justify-center h-full w-3/5 border-2 border-[#272728] ">
+      <div className="bg-[#1c1b1c] p-5 rounded-xl item-center justify-center w-full h-96 border-2 border-[#3a3a3a]">
+        <p className="text-gray-300 mb-2 justify-center font-bold">All the links would be posted here</p>
+        <ul className="mt-4 flex-grow overflow-y-auto">
+          {links.map((link, index) => (
+            <li key={index} className="text-white mt-2">
+              {link}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={handleAddLink} className="mt-20 rounded-lg flex space-x-4">
+        <input
+          name="link-input"
+          className="text-gray-300 rounded-xl p-2 pl-5 w-full border-2 border-[#3a3a3a] bg-gradient-to-r from-[#0F0F0F] to-[#2E2E2E] font-semibold"
+          placeholder="Add a link or text"
+        />
+        <button type="submit" className=" text-white rounded-xl p-3 border-2 border-[#3a3a3a] bg-[#272728]">
+          <img src="Icon.svg" alt="Add" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LinkBox;
