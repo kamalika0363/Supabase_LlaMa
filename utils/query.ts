69553556ccbc988025a08bb1f5561785523c331c
kamalika0363 <kamalikaghora14@gmail.com>
2023-12-20 01:09:31 +0530

    env updates

--- utils/query.ts
+++ utils/query.ts
@@ -4,7 +4,7 @@ export async function query(data: any) {
     const response = await fetch(
         "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
         {
-            headers: { Authorization: "Bearer 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" },
+            headers: { Authorization: `Bearer ${process.env.HF_KEY}` },
             method: "POST",
             body: JSON.stringify(data),
         }
